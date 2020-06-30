58bc753dbb7c831ab0338aa9a97d5ce62b9523f2
Matt Kimber <github@mattkimber.org.uk>
2020-06-30 19:24:54 +0100

    more rotation sprites (test)

--- roadie.sh
+++ roadie.sh
@@ -3,5 +3,5 @@
 echo "Compiling set"
 ../roadie/roadie.exe set.json
 echo "Compiling NML"
-../nml/nmlc.exe -c timberwolfs_trains.nml
-#../nml/nmlc.exe timberwolfs_trains.nml
+#../nml/nmlc.exe -c timberwolfs_trains.nml
+../nml/nmlc.exe timberwolfs_trains.nml
