ae286676e6e65c69889845972f55622037ecae23
Matt Kimber <github@mattkimber.org.uk>
2020-07-19 15:46:39 +0100

    hill sprites

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Create directories
+mkdir -p intermediate
+mkdir -p intermediate/hills
+
 # Do cargo and animation compositing
-../cargopositor/cargopositor.exe -o intermediate -v voxels -t positor/*
+../cargopositor/cargopositor.exe -o intermediate -v voxels -t positor/first/*
 
 # Copy static files
 cp voxels/static/mu/* intermediate
@@ -9,6 +13,9 @@ cp voxels/static/carriage/* intermediate
 cp voxels/static/loco/* intermediate
 cp voxels/static/tender/* intermediate
 cp voxels/static/wagon/* intermediate
+
+# Do hill sprite creation
+../cargopositor/cargopositor.exe -o intermediate/hills -v intermediate -t positor/second/*
 
 for i in `ls intermediate`; do 
     fn=`echo 2x/${i}_32bpp.png | sed -e s/.vox//`
@@ -18,6 +25,17 @@ for i in `ls intermediate`; do
     else
         echo "$i [new]"
 	    ../gorender/renderobject.exe -i intermediate/$i -o $i -s 2,1 -u
+    fi
+done
+
+for i in `ls intermediate/hills`; do 
+    fn=`echo 2x/${i}_32bpp.png | sed -e s/.vox//`
+
+    if [ -e $fn ]; then 
+        echo "$i [cached]"
+    else
+        echo "$i [new]"
+	    ../gorender/renderobject.exe -i intermediate/hills/$i -m files/manifest_hill.json -o $i -s 2,1 -u
     fi
 done
 
