e516cce7b15fd66c0291a34ba5a033f97b01c25f
Matt Kimber <github@mattkimber.org.uk>
2020-08-02 19:15:13 +0100

    fix tunnel glitches

--- build.sh
+++ build.sh
@@ -5,35 +5,52 @@ mkdir -p intermediate
 mkdir -p intermediate/hills
 
 # Do cargo and animation compositing
+echo "Compositing sprites"
 ../cargopositor/cargopositor.exe -o intermediate -v voxels -t positor/first/*
 
 # Copy static files
+echo "Copying static files"
+
 cp voxels/static/mu/* intermediate
 cp voxels/static/carriage/* intermediate
 cp voxels/static/loco/* intermediate
 cp voxels/static/tender/* intermediate
 cp voxels/static/wagon/* intermediate
 
-# Do hill sprite creation
+# Do sprite creation
+echo "Compositing hill sprites"
 ../cargopositor/cargopositor.exe -o intermediate/hills -v intermediate -t positor/second/*
 
-for i in `ls intermediate`; do 
-    fn=`echo 2x/${i}_32bpp.png | sed -e s/.vox//`
+echo "Rendering regular sprites"
 
-    if [ -e $fn ]; then 
-        echo "$i [cached]"
-    else
+for i in `ls intermediate`; do 
+    fn=`echo 2x/${i}_8bpp.png | sed -e s/.vox//`
+
+    if [ ! -e $fn ]; then 
         echo "$i [new]"
 	    ../gorender/renderobject.exe -i intermediate/$i -o $i -s 2,1 -u
     fi
 done
 
-for i in `ls intermediate/hills`; do 
-    fn=`echo 2x/${i}_32bpp.png | sed -e s/.vox//`
+echo "Rendering section sprites"
 
-    if [ -e $fn ]; then 
-        echo "$i [cached]"
-    else
+for i in `ls intermediate`; do 
+    fn=`echo 2x/${i}_sections_8bpp.png | sed -e s/.vox//`
+
+    if [ ! -e $fn ]; then 
+        f=`echo $i | sed -e s/.vox//`
+        echo "$f [new]"
+        ../gorender/renderobject.exe -m files/manifest_sections.json  -i intermediate/$i -o ${f}_sections -s 2,1 -u
+    fi
+done
+
+
+echo "Rendering hill sprites"
+
+for i in `ls intermediate/hills`; do 
+    fn=`echo 2x/${i}_8bpp.png | sed -e s/.vox//`
+
+    if [ ! -e $fn ]; then 
         echo "$i [new]"
 	    ../gorender/renderobject.exe -i intermediate/hills/$i -m files/manifest_hill.json -o $i -s 2,1 -u
     fi
