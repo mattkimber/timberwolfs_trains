6fe7a12e7af807fae002e97e0359f161532a506a
Matt Kimber <github@mattkimber.org.uk>
2020-08-07 00:50:27 +0100

    updates and alignments

---
+++ produce_templates.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+../temporarily_late/main.exe files/manifest.json files/manifest_sections.json  > templates/static/sprite_templates.nml
+../temporarily_late/main.exe files/manifest_hill.json > templates/static/sprite_templates_hills.nml
