649ca7835b5dc81cf1221b9b5d56d7d92179ef2e
Matt Kimber <github@mattkimber.org.uk>
2021-02-14 16:50:00 +0000

    Decapod and tdrl fixes

--- produce_templates.sh
+++ produce_templates.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ../temporarily_late/main.exe files/manifest.json files/manifest_sections.json  > templates/static/sprite_templates.nml
 ../temporarily_late/main.exe files/manifest_hill.json > templates/static/sprite_templates_hills.nml
+
+
+../temporarily_late/main.exe -anim files/manifest.json files/manifest_sections.json  > templates/static/sprite_templates_anim.nml
+../temporarily_late/main.exe -anim files/manifest_hill.json > templates/static/sprite_templates_hills_anim.nml
